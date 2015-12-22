3e84a17a4523b5327a0e3d9b64906652b7da3f86
Christian Roman <chroman16@gmail.com>
2015-12-21 20:00:28 -0600

    No brew cask apps

--- homebrew.sh
+++ homebrew.sh
@@ -46,8 +46,8 @@ brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-libass -
 brew install lftp
 brew install optipng
 brew install pv
-brew install node
-brew install go
+#brew install node
+#brew install go
 brew install base64
 brew install dockutil
 brew install nmap
@@ -57,75 +57,74 @@ brew install postgresql
 brew install webkit2png
 brew install graphicsmagick
 brew install ssh-copy-id
-brew install heroku-toolbelt
 
 brew tap homebrew/dupes
 brew install homebrew/dupes/grep
 
-echo "Installing Homebrew Cask apps..."
+#echo "Installing Homebrew Cask apps..."
 
 # Apps
-brew install caskroom/cask/brew-cask
+#brew install caskroom/cask/brew-cask
 
 # Essentials
 
-brew cask install onepassword
-brew cask install skype
-brew cask install dropbox
-brew cask install google-chrome
-brew cask install firefox
-brew cask install spotify
-brew cask install flash
-brew cask install the-unarchiver
-brew cask install vlc
-brew cask install odesk-team
-brew cask install on-the-job
-brew cask install transmission
-brew cask install java
-brew cask install knox
+#brew cask install onepassword
+#brew cask install skype
+#brew cask install dropbox
+#brew cask install google-chrome
+#brew cask install firefox
+#brew cask install spotify
+#brew cask install flash
+#brew cask install the-unarchiver
+#brew cask install vlc
+#brew cask install odesk-team
+#brew cask install on-the-job
+#brew cask install transmission
+#brew cask install java
+#brew cask install knox
 
 # Misc tools
-brew cask install appzapper
-brew cask install appcleaner
-brew cask install imageoptim
-brew cask install flux
-brew cask install mou
-brew cask install cakebrew
-brew cask install little-snitch
-brew cask install spectacle
-brew cask install wireshark
-brew cask install virtualbox
-brew cask install screenflick
-brew cask install transmit
-brew cask install xquartz
+#brew cask install appzapper
+#brew cask install appcleaner
+#brew cask install imageoptim
+#brew cask install flux
+#brew cask install mou
+#brew cask install cakebrew
+#brew cask install little-snitch
+#brew cask install spectacle
+#brew cask install wireshark
+#brew cask install virtualbox
+#brew cask install screenflick
+#brew cask install transmit
+#brew cask install xquartz
 
 # Dev tools
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install sourcetree
-brew cask install snippets
-brew cask install quickradar
-brew cask install paw
-brew cask install simpholders
-brew cask install android-studio
-brew cask install codekit
-brew cask install sequel-pro
-brew cask install dash
-brew cask install cactus
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install kaleidoscope
-brew cask install hex-fiend
+#brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
+#brew cask install iterm2
+#brew cask install sublime-text
+#brew cask install sourcetree
+#brew cask install snippets
+#brew cask install quickradar
+#brew cask install paw
+#brew cask install simpholders
+#brew cask install android-studio
+#brew cask install codekit
+#brew cask install sequel-pro
+#brew cask install dash
+#brew cask install cactus
+#brew cask install vagrant
+#brew cask install vagrant-manager
+#brew cask install kaleidoscope
+#brew cask install hex-fiend
 
 # Design tools
-brew cask install sketch
-brew cask install colorpicker-skalacolor
-brew cask install inboard
+#brew cask install sketch
+#brew cask install colorpicker-skalacolor
+#brew cask install inboard
 
 echo "Finalizing Homebrew configuration..."
 
 brew update
 brew upgrade
 brew cleanup
-brew cask cleanup
+#brew cask cleanup
